a577ec533a08bf782b5cb1f5a8626fe4b410c8d8
Moimolcp <moimolcp@gmail.com>
2018-09-29 22:48:49 -0500

    bug fix

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ from pymongo import MongoClient
 DATABASE = MongoClient()['restfulapi'] # DB_NAME
 DEBUG = True
 #client = MongoClient('localhost', 27017)
-client = MongoClient('mongodb://datastore', 27017)
+client = MongoClient('mongodb://booklist-db', 27017)
